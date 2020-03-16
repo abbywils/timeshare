11554ac06ce367339296ea15f45e340aa1b9a3a0
abbywils <abby.othman@outlook.com>
2020-03-16 19:48:32 +0000

    configuring leaflet maps

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,23 @@
 class Site < ApplicationRecord
   validates :sitename, :description,  :country_id, :dateinscribed, :longitude, :latitude, :area, :sitetype, :countryname, presence: true
   belongs_to :country
+  
+
+def self.search(sitetype: "", country_id: "", region: "")
+    results = Site.all
+    
+    if !sitetype.blank?
+      results = results.where("sitetype = ?", sitetype)
+    end
+    
+    if !country_id.blank?
+      results = results.where("country_id = ?", country_id)
+    end
+    
+    if !region.blank? 
+      results = results.joins(:country).where("countries.region = ?", region)
+    end
+      
+    return results.order(sitename: :asc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :countries
   get 'timeshare/index'
 
-  root 'volcanomappr#index'
+  root 'timeshare#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
