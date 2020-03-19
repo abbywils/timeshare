66a68b601913b5df866701df10a8d15f6ba537f9
abbywils <abby.othman@outlook.com>
2020-03-19 16:41:47 +0000

    adding users and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  resources :users
   resources :sites
   resources :countries
   get 'timeshare/index'
 
-  root 'timeshare#index'
+   root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_112927) do
+ActiveRecord::Schema.define(version: 2020_03_19_152834) do
 
   create_table "countries", force: :cascade do |t|
     t.string "countryname"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_03_16_112927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["country_id"], name: "index_sites_on_country_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
