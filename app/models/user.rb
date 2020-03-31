cdfc614d77ba83614438beb1684594c78782985b
abbywils <abby.othman@outlook.com>
2020-03-31 09:10:00 +0000

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length:{maximum: 50 }
     VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 75 },
-                     format: { with: VALID_EMAIL_REGEX },
+                     format: { with: URI::MailTo::EMAIL_REGEXP },
                      uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6}
